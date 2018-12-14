a7ecd51bd1635d5947f64fc6c11d263730341593
Jacob Ouellette <jackscripter45@gmail.com>
2018-12-13 20:17:43 -0500

    v0.3
    - Support CNAME and MX records
    - Code optimization

--- nayt.sh
+++ nayt.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-# v0.2
+# v0.3
 ##### COLOR CODE #####
 declare -r DEF='\e[0m'          # Default
 declare -r RED='\e[31m'         # Red
 declare -r GREEN='\e[92m'       # Green
 ######################
 
-# Check first arg
 function Help() {
-        echo "To create an A record and create a PTR record in reverse zone automatically, use the first synthax."
-        echo "Synthax 1: ./nayt.sh -r zone.ca Host_IP Hostname"
-        echo "The zone file name MUST BE the same as the domain name. The FQDN will be automatically set to HOSTNAME.ZONENAME. so don't use FQDN as hostname."
-}
-if [[ "$1" =~ '.' ]]; then ZONE="$1"; RECORD="$2"; HOST="$3"; BIND="$4"; MODE=1                                 # Don't update reverse zone.
-elif [[ "$1" == "-r" ]]; then ZONE="$2"; ZONE_REV="${ZONE}.rev"; RECORD='A'; HOST="$3"; BIND="$4"; MODE=2       # Update reverse zone.
-elif [[ "$1" =~ "-h" ]]; then Help
-else echo -e "INVALID SYNTHAX\nSynthax 1: nayt.sh -r zone.ca IPv4 hostname\nSynthax 2: nayt.sh zone.ca [A/MX/CNAME] IP/host bind/FQDN"; fi
+        echo -e "\nTo create an A record and create a PTR record in reverse zone automatically, use the first syntax.\n"
+        echo "Syntax 1: ./nayt.sh -r zone.ca Hostname IP"
+        echo "Syntax 2: ./nayt.sh zone.ca [A/CNAME] CName Hostname"
+        echo -e "Syntax 3: ./nayt.sh zone.ca MX Hostname\n"
+        echo "The zone file name MUST BE the same as the domain name."
+        echo "When -r or MX is specified, the FQDN will be automatically set to HOSTNAME.ZONENAME. so don't use FQDN as hostname."
+} #0.3
+if [[ "$1" =~ '.' ]]; then ZONE="$1"; RECORD="$2"; BIND="$3"; if ! [[ "$RECORD" == "MX" ]]; then HOST="$4"; fi; MODE=1   #0.3: Don't update reverse zone.
+elif [[ "$1" == "-r" ]]; then ZONE="$2"; ZONE_REV="${ZONE}.rev"; RECORD='A'; HOST="$4"; BIND="$3"; MODE=2       # Update reverse zone.
+elif [[ "$1" =~ "-h" ]]; then Help; exit 1
+else Help; exit 1; fi
 
 possibleOS=`cat /etc/os-release | grep '^ID' | cut -d'=' -f2`
 if [[ "$possibleOS" =~ "centos" ]]; then declare -r OS="centos"; declare -r ZONE_PATH='/var/named'
@@ -25,47 +27,42 @@ function Error() {
         case $1 in
                 1) echo -e "${RED}ERROR - Forwarding zone file $ZONE_PATH/$ZONE cannot be found.${DEF}";;
                 2) echo -e "${RED}ERROR - Reverse zone file $ZONE_PATH/$ZONE_REV cannot be found.${DEF}";;
-                3) echo -e "${RED}ERROR - Invalid IPv4 address.${DEF}";;
+                3) echo -e "${RED}ERROR - Invalid record type.${DEF}";;
+                4) echo -e "${RED}Dude, I can't write to $ZONE_PATH/$ZONE. Give me access !${DEF}";;
+                5) echo -e "${RED}I need to write into $ZONE_PATH/$ZONE_REV but I can't !${DEF}";;
         esac
-        exit 0
-}
+        exit 1
+} #0.3
 if ! [ -f $ZONE_PATH/$ZONE ]; then Error 1; fi
 if ! [ -f $ZONE_PATH/$ZONE_REV ] && [[ "$1" == "-r" ]]; then Error 2; fi
-function VerifyIP() {
-        IFS='.' inarr=(${1});
-        appender=''
-        for i in {0..3}; do
-                if ! [[ ${inarr[i]} =~ $INT ]]; then Error 3 ${inarr[i]}; fi
-                if [[ $i == 0 || $i == 3 ]]; then
-                        if [[ ${inarr[i]} -le 0 || ${inarr[i]} -ge 255 ]]; then Error 3; fi
-                fi # Check first and last digit, can't be 0 or 255
-                if [[ $i == 1 || $i == 2 ]]; then
-                        if [[ ${inarr[i]} -lt 0 || ${inarr[i]} -gt 255 ]]; then Error 3; fi
-                fi # Allow the 2nd and 3th digit to be 0 or 255
-                appender+=${inarr[$i]}.
-        done # Check IP validation
-}
+function IncrementSerial() {
+        LINE=`grep -ni "; serial" $1 | cut -d':' -f1` # Line that contain the serial
+        SERIAL=`grep -i "; serial" $1 | sed -r 's/([^0-9]*([0-9]*)){1}.*/\1/'`
+        NEWSERIAL=$(($SERIAL+1))
+        sed -i "${LINE}s/$SERIAL/$NEWSERIAL/" $1
+} #0.3
 function ForwardReverse() {
         # Forwarding
-        echo -e "${BIND}.${ZONE}.\tIN\tA\t$HOST" >> $ZONE_PATH/$ZONE && echo -e "Forwarding file\t[$GREEN OK$DEF ]" || echo -e "Forwarding file\t[$RED FAILED$DEF ]" # Create A record in forwarding file zone
-        LINE=`grep -ni "; serial" $ZONE_PATH/$ZONE | cut -d':' -f1` # Line that contain the serial
-        SERIAL=`grep -i "; serial" $ZONE_PATH/$ZONE | sed -r 's/([^0-9]*([0-9]*)){1}.*/\1/'` #0.2
-        NEWSERIAL=$(($SERIAL+1))
-        sed -i "${LINE}s/$SERIAL/$NEWSERIAL/" $ZONE_PATH/$ZONE
+        #echo -e "$BIND\tIN\tA\t$HOST" >> $ZONE_PATH/$ZONE  # Create A record in forwarding file zone
+        echo -e "${BIND}.${ZONE}.\tIN\tA\t$HOST" >> $ZONE_PATH/$ZONE #0.3: Auto add FQDN
+        IncrementSerial "$ZONE_PATH/$ZONE"
         # Reverse
         hostIP=`echo $HOST | cut -d'.' -f4`
-        echo -e "$hostIP\tIN\tPTR\t${BIND}.${ZONE}." >> $ZONE_PATH/$ZONE_REV && echo -e "Reverse file\t[$GREEN OK$DEF ]" || echo -e "Reverse file\t[$RED FAILED$DEF ]"       # Associate A record to a PTR in reverse zone.
-        LINE=`grep -ni "; serial" $ZONE_PATH/$ZONE_REV | cut -d':' -f1` # Line that contain the serial
-        SERIAL=`grep -i "; serial" $ZONE_PATH/$ZONE_REV | sed -r 's/([^0-9]*([0-9]*)){1}.*/\1/'` #0.2
-        NEWSERIAL=$(($SERIAL+1))
-        sed -i "${LINE}s/$SERIAL/$NEWSERIAL/" $ZONE_PATH/$ZONE_REV
+        echo -e "$hostIP\tIN\tPTR\t${BIND}.${ZONE}." >> $ZONE_PATH/$ZONE_REV       # Associate A record to a PTR in reverse zone.
+        IncrementSerial "$ZONE_PATH/$ZONE_REV"
 }
+function Forward() {
+        if [[ "$RECORD" == "CNAME" ]]; then echo -e "$BIND\tIN\tCNAME\t$HOST" >> $ZONE_PATH/$ZONE && IncrementSerial "$ZONE_PATH/$ZONE"
+        elif [[ "$RECORD" == "A" ]]; then echo -e "$BIND\tIN\tA\t$HOST" >> $ZONE_PATH/$ZONE && IncrementSerial "$ZONE_PATH/$ZONE"
+        elif [[ "$RECORD" == "MX" ]]; then echo -e "\tIN\tMX\t10\t${BIND}.${ZONE}." >> $ZONE_PATH/$ZONE && IncrementSerial "$ZONE_PATH/$ZONE"
+        else Error 3; fi
+} #0.3
 function Main() {
-        # Synthax 1: nayt.sh -r zone.ca host bind               # Only for A record.
-        # Synthax 2: nayt.sh zone.ca [A/MX/CNAME] host bind     # Only for forwarding zone.
-        #VerifyIP $HOST
-        #IP=${appender::-1} # Remove last character .
-        if [[ $MODE == 2 ]]; then ForwardReverse; fi
-        #if [[ $MODE == 1 ]]; then
+        # Syntax 1: nayt.sh -r zone.ca host bind               # Only for A record.
+        # Syntax 2: nayt.sh zone.ca [A/CNAME] host bind        # Only for forwarding zone.
+        # Syntax 3: nayt.sh zone.ca MX bind                    # Only for forwarding zone and a MX.
+        if ! [ -w "$ZONE_PATH/$ZONE" ]; then Error 4; fi        # Check if file is writable
+        if [[ $MODE == 2 ]]; then if ! [ -w "$ZONE_PATH/$ZONE_REV" ]; then Error 5; fi; ForwardReverse
+        elif [[ $MODE == 1 ]]; then Forward; fi         #0.3
 }
 Main
